ab89bd9f5a3131140f2e41b2bc94ccc4c85ac74b
Mahmood Ali <Mahmood Ali>
2016-12-05 18:21:54 +0300

    updata and add quize

---
+++ Compiler/Quize/استخراج اول حرف من اول كلمة/Mahmood Ali/fistLetterInFisrtWord/fistLetterInFisrtWord/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace fistLetterInFisrtWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(25, 51);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(247, 20);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "Welcome in Computer Science";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(71, 92);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(147, 34);
+            this.btnGet.TabIndex = 1;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 171);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnGet;
+    }
+}
+
---
+++ Compiler/Quize/استخراج اول حرف من اول كلمة/Mahmood Ali/fistLetterInFisrtWord/fistLetterInFisrtWord/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fistLetterInFisrtWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            string[] words = txtInput.Text.Split(' ');
+
+            string firstWord=words[0];
+                  
+            char firstLetter = firstWord.Length > 0 ? firstWord[0] : ' ' ;
+
+            MessageBox.Show(firstLetter.ToString());
+
+            // Or 
+
+           // MessageBox.Show(words[0][0].ToString());
+        }
+
+
+       
+    }
+}
