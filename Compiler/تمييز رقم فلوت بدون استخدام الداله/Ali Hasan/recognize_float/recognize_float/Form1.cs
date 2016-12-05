039e5f792ae504f7fb4e06609a45442f98afc8c3
alihasan95 <ali00.hasan00@gmail.com>
2016-12-05 16:18:04 +0300

    lab5

---
+++ Compiler/تمييز رقم فلوت بدون استخدام الداله/Ali Hasan/recognize_float/recognize_float/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace recognize_float
+{
+    public partial class Form1 : Form
+    {
+        public int i;
+        public int x;
+        string[] num;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] wrords = textBox1.Text.Split(' ');
+
+            for (i = 0; i < wrords.Length; i++)
+            {
+                if (wrords[i].Contains('.'))
+                {
+                    num = wrords[i].Split('.');
+                    foreach (string n in num)
+                    {
+                        int.TryParse(n, out x);
+                        listBox1.Items.Add(x);
+
+                    }
+                }
+
+            }          
