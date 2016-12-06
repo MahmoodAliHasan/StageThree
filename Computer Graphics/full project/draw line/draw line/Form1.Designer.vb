6a4aa254a8ce95cebcc69a61954ed8708dff7881
alihasan95 <ali00.hasan00@gmail.com>
2016-12-06 16:56:22 +0300

    commit

---
+++ Computer Graphics/full project/draw line/draw line/Form1.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBoxX1 = New System.Windows.Forms.TextBox()
+        Me.TextBoxX2 = New System.Windows.Forms.TextBox()
+        Me.TextBoxY1 = New System.Windows.Forms.TextBox()
+        Me.TextBoxY2 = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Labelx = New System.Windows.Forms.Label()
+        Me.Labely = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.LineToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DirectToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DDAToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BresenhumToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExamplesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HomeWorksToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HouseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 153)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(401, 213)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBoxX1
+        '
+        Me.TextBoxX1.Location = New System.Drawing.Point(12, 51)
+        Me.TextBoxX1.Name = "TextBoxX1"
+        Me.TextBoxX1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxX1.TabIndex = 1
+        '
+        'TextBoxX2
+        '
+        Me.TextBoxX2.Location = New System.Drawing.Point(130, 51)
+        Me.TextBoxX2.Name = "TextBoxX2"
+        Me.TextBoxX2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxX2.TabIndex = 2
+        '
+        'TextBoxY1
+        '
+        Me.TextBoxY1.Location = New System.Drawing.Point(12, 91)
+        Me.TextBoxY1.Name = "TextBoxY1"
+        Me.TextBoxY1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxY1.TabIndex = 3
+        '
+        'TextBoxY2
+        '
+        Me.TextBoxY2.Location = New System.Drawing.Point(130, 91)
+        Me.TextBoxY2.Name = "TextBoxY2"
+        Me.TextBoxY2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxY2.TabIndex = 4
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(19, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "x1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(127, 34)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(19, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "x2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 74)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(19, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "y1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(127, 74)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(19, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "y2"
+        '
+        'Labelx
+        '
+        Me.Labelx.AutoSize = True
+        Me.Labelx.Location = New System.Drawing.Point(278, 51)
+        Me.Labelx.Name = "Labelx"
+        Me.Labelx.Size = New System.Drawing.Size(38, 13)
+        Me.Labelx.TabIndex = 13
+        Me.Labelx.Text = "Label5"
+        '
+        'Labely
+        '
+        Me.Labely.AutoSize = True
+        Me.Labely.Location = New System.Drawing.Point(281, 91)
+        Me.Labely.Name = "Labely"
+        Me.Labely.Size = New System.Drawing.Size(38, 13)
+        Me.Labely.TabIndex = 14
+        Me.Labely.Text = "Labely"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LineToolStripMenuItem, Me.ExamplesToolStripMenuItem, Me.HomeWorksToolStripMenuItem, Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(425, 24)
+        Me.MenuStrip1.TabIndex = 15
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'LineToolStripMenuItem
+        '
+        Me.LineToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DirectToolStripMenuItem, Me.DDAToolStripMenuItem, Me.BresenhumToolStripMenuItem})
+        Me.LineToolStripMenuItem.Name = "LineToolStripMenuItem"
+        Me.LineToolStripMenuItem.Size = New System.Drawing.Size(41, 20)
+        Me.LineToolStripMenuItem.Text = "Line"
+        '
+        'DirectToolStripMenuItem
+        '
+        Me.DirectToolStripMenuItem.Name = "DirectToolStripMenuItem"
+        Me.DirectToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.DirectToolStripMenuItem.Text = "Direct "
+        '
+        'DDAToolStripMenuItem
+        '
+        Me.DDAToolStripMenuItem.Name = "DDAToolStripMenuItem"
+        Me.DDAToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.DDAToolStripMenuItem.Text = "DDA"
+        '
+        'BresenhumToolStripMenuItem
+        '
+        Me.BresenhumToolStripMenuItem.Name = "BresenhumToolStripMenuItem"
+        Me.BresenhumToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.BresenhumToolStripMenuItem.Text = "Bresenhum"
+        '
+        'ExamplesToolStripMenuItem
+        '
+        Me.ExamplesToolStripMenuItem.Name = "ExamplesToolStripMenuItem"
+        Me.ExamplesToolStripMenuItem.Size = New System.Drawing.Size(68, 20)
+        Me.ExamplesToolStripMenuItem.Text = "Examples"
+        '
+        'HomeWorksToolStripMenuItem
+        '
+        Me.HomeWorksToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HouseToolStripMenuItem})
+        Me.HomeWorksToolStripMenuItem.Name = "HomeWorksToolStripMenuItem"
+        Me.HomeWorksToolStripMenuItem.Size = New System.Drawing.Size(88, 20)
+        Me.HomeWorksToolStripMenuItem.Text = "Home Works"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(46, 20)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'HouseToolStripMenuItem
+        '
+        Me.HouseToolStripMenuItem.Name = "HouseToolStripMenuItem"
+        Me.HouseToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.HouseToolStripMenuItem.Text = "House"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(322, 34)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(90, 95)
+        Me.ListBox1.TabIndex = 16
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(425, 378)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.Labely)
+        Me.Controls.Add(Me.Labelx)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxY2)
+        Me.Controls.Add(Me.TextBoxY1)
+        Me.Controls.Add(Me.TextBoxX2)
+        Me.Controls.Add(Me.TextBoxX1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBoxX1 As TextBox
+    Friend WithEvents TextBoxX2 As TextBox
+    Friend WithEvents TextBoxY1 As TextBox
+    Friend WithEvents TextBoxY2 As TextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Labelx As Label
+    Friend WithEvents Labely As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents LineToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DirectToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DDAToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BresenhumToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExamplesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HomeWorksToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HouseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Computer Graphics/full project/draw line/draw line/Form1.vb
@@ -0,0 +1,164 @@
+﻿Imports System.Threading
+
+Public Class Form1
+
+    Dim x1, x2, y1, y2 As Integer
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseMove
+        Labelx.Text = e.Location.X
+        Labely.Text = e.Location.Y
+    End Sub
+    Dim flag As Boolean = True
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+        If flag = True Then
+            TextBoxX1.Text = Labelx.Text
+            TextBoxY1.Text = Labely.Text
+            flag = False
+        ElseIf flag = False Then
+            TextBoxX2.Text = Labelx.Text
+            TextBoxY2.Text = Labely.Text
+            flag = True
+        End If
+    End Sub
+
+    Private Sub DirectToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DirectToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub DDAToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DDAToolStripMenuItem.Click
+
+        x1 = Int32.Parse(TextBoxX1.Text)
+        x2 = Int32.Parse(TextBoxX2.Text)
+        y1 = Int32.Parse(TextBoxY1.Text)
+        y2 = Int32.Parse(TextBoxY2.Text)
+
+        DrawnLineDDA(x1, x2, y1, y2)
+    End Sub
+
+    Private Sub BresenhumToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BresenhumToolStripMenuItem.Click
+
+        x1 = Int32.Parse(TextBoxX1.Text)
+        x2 = Int32.Parse(TextBoxX2.Text)
+        y1 = Int32.Parse(TextBoxY1.Text)
+        y2 = Int32.Parse(TextBoxY2.Text)
+
+        BresenhumLine(x1, x2, y1, y2)
+    End Sub
+
+
+    Private Sub DrawnLineDDA(ByVal x1 As Integer, ByVal x2 As Integer, ByVal y1 As Integer, ByVal y2 As Integer)
+        ListBox1.Items.Add(x1.ToString + "   " + y1.ToString)
+        ListBox1.Items.Add(x2.ToString + "   " + y2.ToString)
+
+        Dim graph As Graphics = PictureBox1.CreateGraphics()
+
+
+        Dim length, Dy, Dx, Xinc, Yinc
+
+        Dy = x2 - x1
+        Dx = y2 - y1
+
+
+        length = Math.Max(Math.Abs(Dy), Math.Abs(Dx))
+
+        Xinc = Dx / length
+        Yinc = Dy / length
+
+
+
+        Dim Img As New Bitmap(1, 1)
+        Img.SetPixel(0, 0, Color.Green)
+
+
+        Dim x, y As Double
+        x = x1 + 0.5
+        y = y1 + 0.5
+
+        For i = 1 To length
+
+            graph.DrawImage(Img, Convert.ToInt32(x), Convert.ToInt32(y))
+            x += Xinc
+            y += Yinc
+            Threading.Thread.Sleep(5)
+
+        Next
+    End Sub
+
+
+    Private Sub BresenhumLine(ByVal x1 As Integer, ByVal x2 As Integer, ByVal y1 As Integer, ByVal y2 As Integer)
+
+        ListBox1.Items.Add(x1.ToString + "   " + y1.ToString)
+        ListBox1.Items.Add(x2.ToString + "   " + y2.ToString)
+        Dim dx, dy, p, s1, s2, temp, chang As Integer
+
+
+        Dim x, y As Integer
+
+        Dim graph As Graphics = PictureBox1.CreateGraphics
+
+        Dim image As New Bitmap(1, 1)
+        image.SetPixel(0, 0, Color.Red)
+
+        dx = Math.Abs(x2 - x1)
+        dy = Math.Abs(y2 - y1)
+
+        p = 2 * (dy - dx)
+
+        s1 = Math.Sign(x2 - x1)
+        s2 = Math.Sign(y2 - y1)
+
+        x = x1
+        y = y1
+
+        If (dy > dx) Then
+            temp = dx
+            dx = dy
+            dy = dx
+            chang = 1
+        Else
+            chang = 0
+        End If
+
+        For i = 1 To dx
+
+            graph.DrawImage(image, x, y)
+            If (p >= 0) Then
+                If (chang = 1) Then
+                    x = x + s1
+                Else
+                    y = y + s2
+                End If
+
+                p = p + 2 * (dy - dx)
+            Else
+                p = p + 2 * dy
+            End If
+            If (chang = 1) Then
+                y = y + s2
+            Else
+                x = x + s1
+            End If
+            ' Console.WriteLine(x & "   graph.DrawImage(Img, x, y)
+        Next
+    End Sub
+
+    Private Sub HouseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HouseToolStripMenuItem.Click
+        BresenhumLine(15, 70, 70, 15)
+        BresenhumLine(70, 15, 115, 70)
+        BresenhumLine(15, 70, 120, 70)
+        BresenhumLine(15, 70, 15, 130)
+        BresenhumLine(120, 70, 120, 130)
+        BresenhumLine(15, 130, 120, 130)
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
+        PictureBox1.Image = Nothing
+    End Sub
+
+
+End Class
